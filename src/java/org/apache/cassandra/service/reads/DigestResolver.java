7c602133560e05a2e722dac377dd40791c2fab7e
Raj <pandeyg.raj@gmail.com>
2024-12-08 11:51:21 -0500

    debug

--- src/java/org/apache/cassandra/service/reads/DigestResolver.java
+++ src/java/org/apache/cassandra/service/reads/DigestResolver.java
@@ -273,7 +273,7 @@ public class DigestResolver<E extends Endpoints<E>, P extends ReplicaPlan.ForRea
                 encoded_value = encoded_value + ecResponses[i].getEcCode();
                 logger.info("Combining value:  " + encoded_value);
             }
-            ReadResponse tmpp = modifyCellValue(tmp,encoded_value);// should use trim() ?
+            ReadResponse tmpp = modifyCellValue(tmp,encoded_value.trim());// should use trim() mostly Yes?
             return UnfilteredPartitionIterators.filter(tmpp.makeIterator(command), command.nowInSec());
         }
 
