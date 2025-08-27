8cc87136fd4e402d1b7904270a706ba81dc7d282
Raj <pandeyg.raj@gmail.com>
2025-08-27 12:34:25 -0400

    log

--- src/java/org/apache/cassandra/utils/LatencyRecorder.java
+++ src/java/org/apache/cassandra/utils/LatencyRecorder.java
@@ -69,13 +69,13 @@ public class LatencyRecorder {
      * @param duration Duration in nanoseconds
      */
     public static void record(String type, long duration) {
-
+        /*
         buffer.add(type + "," + duration);
         if (buffer.size() >= BUFFER_SIZE_TRIGGER) {
            flush();
         }
 
-
+         */
     }
 
     /**
