b67919c2111b805851ec9a9b94a753cd8fc38f57
Raj <pandeyg.raj@gmail.com>
2025-08-27 12:01:02 -0400

    log

--- src/java/org/apache/cassandra/utils/LatencyRecorder.java
+++ src/java/org/apache/cassandra/utils/LatencyRecorder.java
@@ -44,11 +44,11 @@ public class LatencyRecorder {
     //private static final ScheduledExecutorService flusher = Executors.newSingleThreadScheduledExecutor();
 
     // Output file path
-    private static final String OUTPUT_FILE = "/tmp/cassandra_latencies.log";
+    private static final String OUTPUT_FILE = "cassandra_latencies.log";
 
     // Flush parameters
     // private static final int FLUSH_INTERVAL_SECONDS = 300;     // periodic flush interval
-    // private static final int BUFFER_SIZE_TRIGGER = 1_000_000; // flush if buffer exceeds this many entries
+     private static final int BUFFER_SIZE_TRIGGER = 1_000_000; // flush if buffer exceeds this many entries
 
     /*
     static {
@@ -72,9 +72,9 @@ public class LatencyRecorder {
         buffer.add(type + "," + duration);
 
         // Flush if buffer exceeds trigger size
-        //if (buffer.size() >= BUFFER_SIZE_TRIGGER) {
-        //   flush();
-        //}
+        if (buffer.size() >= BUFFER_SIZE_TRIGGER) {
+           flush();
+        }
     }
 
     /**
