a6f12ea15fff5b5daee67b0d0aa1ffc5ed0d1d40
RAJ KUMAR PANDEY <pandeyg.raj@gmail.com>
2026-06-12 13:18:25 -0400

    Update ConsistencyLevel.java

--- src/java/org/apache/cassandra/db/ConsistencyLevel.java
+++ src/java/org/apache/cassandra/db/ConsistencyLevel.java
@@ -98,8 +98,8 @@ public enum ConsistencyLevel
     {
         //rajj debug start change quorum size
 
-          //return (replicationStrategy.getReplicationFactor().allReplicas / 2) + 1;
-        return (ECConfig.DATA_SHARDS + ((ECConfig.PARITY_SHARDS + 1 )/ 2));
+          return (replicationStrategy.getReplicationFactor().allReplicas / 2) + 1;
+        //return (ECConfig.DATA_SHARDS + ((ECConfig.PARITY_SHARDS + 1 )/ 2));
 
 
         // int quorum = (replicationStrategy.getReplicationFactor().allReplicas / 2) + 1;
